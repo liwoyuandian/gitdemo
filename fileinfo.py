0d0f71e34cc70953a7378aed8c485c7b64351c27
cxxsxx <2539484334@qq.com>
2020-04-02 16:47:47 +0800

    update by the second developer

--- fileinfo.py
+++ fileinfo.py
@@ -9,3 +9,13 @@ def f(x):
     return n
 m=f(x)
 print('The cyphertext is {}.'.format(m))
+x=int(input())
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+def f(x):
+    n=((x*a)-b)*c+d
+    return n
+m=f(x)
+print('The cyphertext is {}.'.format(m))
