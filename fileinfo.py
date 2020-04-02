2c9af2f5aef8847b6d02cbbb7af3175890db1f67
unknown <1429345720@qq.com>
2020-04-02 15:58:50 +0800

    initial update

---
+++ fileinfo.py
@@ -0,0 +1,11 @@
+# 简单密码
+x=int(input())
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+def f(x):
+    n=((x*a)-b)*c+d
+    return n
+m=f(x)
+print('The cyphertext is {}.'.format(m))
